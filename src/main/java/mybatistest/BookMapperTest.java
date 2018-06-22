b005e1c62f3e10ef7923557e24d8f149588d6d2f
Mr Zhu <451218651@qq.com>
2018-06-22 14:08:33 +0800

    Mybatis - 对多关系的映射（collection）

--- src/main/java/mybatistest/BookMapperTest.java
+++ src/main/java/mybatistest/BookMapperTest.java
@@ -1,6 +1,7 @@
 package mybatistest;
 
 import mybatistest.mapper.BookMapper;
+import mybatistest.pojo.Author;
 import mybatistest.pojo.Book;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,6 +13,12 @@ public class BookMapperTest {
 		BookMapper mapper = context.getBean(BookMapper.class);
 		
 		Book book = mapper.findOne(1L);
-		System.out.println("findOne: " + book.getTitle() + ", " + book.getPublisher().getName());
+		System.out.println("findOne: " + book.getTitle() + ", " + 
+				// 对一关系Book.publisher
+				book.getPublisher().getName());
+		// 对多关系Book.authors
+		for (Author author : book.getAuthors()) {
+			System.out.println(author.getName() + "," + author.getGender());
+		}
 	}
 }
