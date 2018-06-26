c0d464e5f99c7c33ca172b7e25e88f1527fd09aa
Mr Zhu <451218651@qq.com>
2018-06-26 09:43:46 +0800

    练习10 - 参考答案（findAll）

--- src/main/java/mybatistest/AuthorMapperTest.java
+++ src/main/java/mybatistest/AuthorMapperTest.java
@@ -1,5 +1,7 @@
 package mybatistest;
 
+import java.util.List;
+
 import mybatistest.mapper.AuthorMapper;
 import mybatistest.pojo.Author;
 import mybatistest.pojo.Book;
@@ -14,14 +16,28 @@ public class AuthorMapperTest {
 		AuthorMapper mapper = context.getBean(AuthorMapper.class);
 		
 		Author author = mapper.findOne(6L);
-		System.out.println("findOne: " + 
-					String.format("%s[%s]（%s）", 
-							author.getName(), 
-							author.getGender(),
-							author.getDescription()));
+		System.out.println("findOne: ");
+		printAuthor(author);
+		
+		System.out.println();
+		
+		System.out.println("findAll:");
+		List<Author> authors = mapper.findAll();
+		for (Author a : authors) {
+			printAuthor(a);
+		}
+	}
+
+	private static void printAuthor(Author author) {
+		String authorInfo = String.format("%s[%s]（%s）", 
+				author.getName(), 
+				author.getGender(),
+				author.getDescription());
+		System.out.println(authorInfo);
+		
 		// 对多关系author.books
 		for (Book book : author.getBooks()) {
-			System.out.println(String.format("《%s》【%s】（%s）", 
+			System.out.println(String.format("\t《%s》【%s】（%s）", 
 					book.getTitle(),
 					book.getPublisher().getName(),
 					book.getDescription()));
--- src/main/java/mybatistest/mapper/AuthorMapper.java
+++ src/main/java/mybatistest/mapper/AuthorMapper.java
@@ -1,7 +1,11 @@
 package mybatistest.mapper;
 
+import java.util.List;
+
 import mybatistest.pojo.Author;
 
 public interface AuthorMapper {
 	Author findOne(Long id);
+	
+	List<Author> findAll();
 }
